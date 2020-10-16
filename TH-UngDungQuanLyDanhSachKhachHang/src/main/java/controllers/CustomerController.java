595b89d703db30dad89fdaf90f23b46f69932b1e
Do Chanh Tin <@gmail>
2020-10-16 09:01:29 +0700

    update

---
+++ TH-UngDungQuanLyDanhSachKhachHang/src/main/java/controllers/CustomerController.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+    @Controller
+    public class CustomerController {
+        @GetMapping("/customers")
+        public String showList() {
+            return "customers/list";
+        }
+    }
